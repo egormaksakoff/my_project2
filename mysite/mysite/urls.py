832573f7e9e69ee763cc8227ac7ef58dccdf1b19
Вадим Кравченко <kravchenko_vser@pfur.ru>
2023-04-11 20:25:26 +0300

    create input form

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pools.controllers import index
+from pools.controllers import feedback
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('hobbies/', index.hobbies),
     path('gallery/', index.gallery),
     path('photo/', index.photo),
+    path('feedback/', feedback.create),
 ]
